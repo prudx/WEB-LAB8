a8333e438d543fc7dfd21a31e3356c901810d70a
x00131205 <x00131205@gmail.com>
2016-11-10 13:24:53 +0000

    2nd commit, done but not right output

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,16 @@
 package controllers;
+
+import play.api.Environment;
 import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.ArrayLIst;
+import java.util.List;
+import javax.inject.Inject;
+
 import views.html.*;
+import models.*;
 
 public class HomeController extends Controller {
 
@@ -13,7 +23,9 @@ public class HomeController extends Controller {
     }
 
     public Result products() {
-        return ok(products.render());
+        List<products> productsList = Product.findAll();
+        
+        return ok(products.render(productsList));
     }
 
 }
